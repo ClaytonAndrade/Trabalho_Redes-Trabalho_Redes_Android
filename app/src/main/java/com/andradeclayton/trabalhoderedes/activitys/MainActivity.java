ed800fcf5c246c70088bdaafc9e89df7eae7d63c
ClaytonAndrade <clayton_m_andrade@hotmail.com>
2018-11-25 01:54:27 -0200

    sincronizacao, variaveis a e execoes a tratar

--- app/src/main/java/com/andradeclayton/trabalhoderedes/activitys/MainActivity.java
+++ app/src/main/java/com/andradeclayton/trabalhoderedes/activitys/MainActivity.java
@@ -1,26 +1,26 @@
 package com.andradeclayton.trabalhoderedes.activitys;
 
+import android.content.Context;
+import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.andradeclayton.trabalhoderedes.R;
-import com.andradeclayton.trabalhoderedes.models.QuantidadeMedicacao;
-import com.andradeclayton.trabalhoderedes.services.AcessoServidorSockets;
-import com.andradeclayton.trabalhoderedes.services.QuantidadeMedicacaoService;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     Spinner  spn_faixa_etaria;
     Spinner  spn_medicamento;
-    EditText edt_peso_paciente;
+    EditText edt_ip;
     Button   btn_calcular_dosagem;
     TextView txv_resultado;
 
@@ -43,9 +43,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (android.os.Build.VERSION.SDK_INT > 9){
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
         spn_faixa_etaria     = (Spinner)  findViewById(R.id.spinner_faixa_etaria);
         spn_medicamento      = (Spinner)  findViewById(R.id.spinner_tipo_remedios);
-        edt_peso_paciente    = (EditText) findViewById(R.id.edt_peso_paciente);
+        edt_ip               = (EditText) findViewById(R.id.edt_ip);
         btn_calcular_dosagem = (Button)   findViewById(R.id.btn_calcular);
         txv_resultado        = (TextView) findViewById(R.id.txv_resultado);
 
@@ -83,45 +88,87 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /*public String RequestConsulta(QuantidadeMedicacao quantidadeMedicacao){
-
-        final String[] resposta = new String[1];
-        final String respostaRequest = "A medida nào pode ser calculada";
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(QuantidadeMedicacaoService.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        QuantidadeMedicacaoService service = retrofit.create(QuantidadeMedicacaoService.class);
-        Call<String> reCall = service.resultado(quantidadeMedicacao);
-
-        reCall.enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                if(!response.isSuccessful()){
-                    Log.e(TAG, "Erro: " + response.code());
-                }else{
-                     resposta[0] = response.body();
-
-                    //respostaRequest = response.raw();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                Log.e(TAG, "Erro: " + t.getMessage());
-            }
-        });
-
-        return respostaRequest;
-    }*/
-
     public void CalcularDosagem(View view) {
-        AcessoServidorSockets acessoServidorSockets = new AcessoServidorSockets();
-        String mensagem;
+        final String ip;
+        final String mensagem;
+        boolean validacaoip = false;
 
-        mensagem = faixa_etaria + " " + medicamento + " " + edt_peso_paciente.getText().toString();
-        acessoServidorSockets.sendMessage(mensagem);
+        hideKeyboard(this, view);
+
+        if(!validaIp()){
+            return;
+        }
+        mensagem = faixa_etaria + " " + medicamento;
+        ip = edt_ip.getText().toString();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                conectarSocket(mensagem, ip);
+            }
+        }).start();
     }
+
+    private void conectarSocket(String msg, String ip) {
+        Socket socket = null;
+        String mens = "Nào conseguimos manter uma conexão";
+        DataOutputStream canalSaida = null;
+        DataInputStream canalEntrada = null;
+
+        try {
+
+            socket = new Socket(ip, 8080);
+
+            canalSaida = new DataOutputStream(socket.getOutputStream());
+            canalSaida.writeUTF(msg);
+
+            canalEntrada = new DataInputStream(socket.getInputStream());
+            mens = canalEntrada.readLine();
+            txv_resultado.setText(mens);
+
+
+        } catch (Exception e) {
+            //FIXME Tratar a Exception.
+            e.printStackTrace();
+        }finally {
+            try{
+                if(socket != null){
+                    socket.close();
+                }
+                if(canalSaida != null){
+                    canalSaida.close();
+                }
+                if(canalEntrada != null){
+                    canalEntrada.close();
+                }
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean validaIp(){
+
+        if(edt_ip.getText().toString().equals("")){
+            AlertDialog alerta;
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Atenção");
+            builder.setMessage("O campo IP deve ser configurado");
+            alerta = builder.create();
+            alerta.show();
+
+            edt_ip.requestFocus();
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void hideKeyboard(Context context, View editText) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
+
 }
